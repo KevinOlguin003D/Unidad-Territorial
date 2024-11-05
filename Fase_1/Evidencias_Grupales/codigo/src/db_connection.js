50129183ff5a27989423bdfd056916f68c805fe6
Fabian Cortes <fa.cortesp@duocuc.cl>
2024-11-05 17:02:47 -0300

    Nueva conexión a bbdd

--- Fase_1/Evidencias_Grupales/codigo/src/db_connection.js
+++ Fase_1/Evidencias_Grupales/codigo/src/db_connection.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
 const mysql = require('mysql2');
 //CONECTAR USUARIO A LA BBDD
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: '144.22.36.128',  
+  user: 'sut_user',
+  password: 'DB12345__',  
+  database: 'proyectosut'
 });
 
 connection.connect(err => {
